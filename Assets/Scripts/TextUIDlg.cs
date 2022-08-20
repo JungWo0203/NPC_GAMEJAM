124ffdf0e16cae3fc5ab5f3aa63c5ee431923dda
JungWo0203 <102477444+JungWo0203@users.noreply.github.com>
2022-08-21 07:14:41 +0900

    111
    음악

--- Assets/Scripts/TextUIDlg.cs
+++ Assets/Scripts/TextUIDlg.cs
@@ -13,6 +13,7 @@ public class TextUIDlg : MonoBehaviour
     public Button m_select1;
     public Button m_select2;
     public Button m_selectSingle;
+    public EndingScript m_endingScript = null;
 
     public void Initialize()
     {
@@ -30,15 +31,41 @@ public class TextUIDlg : MonoBehaviour
     public void OnClick_Select1()
     {
         int connectedId = TextMgr.Inst.m_assetTexts[m_currentid].connectedid;
-        m_currentid = m_currentid + connectedId+1;
-        DescTextChange();
+        if (TextMgr.Inst.m_assetTexts[m_currentid - 1].category==3)
+        {
+            m_endingScript.PrintEndingCredit(1);
+            //비활성화
+        }
+        else
+        {
+            m_currentid = m_currentid + connectedId + 1;
+            DescTextChange();
+        }
+
     }
     public void OnClick_Select2()
     {
         int connectedId = TextMgr.Inst.m_assetTexts[m_currentid +1].connectedid;
-        m_currentid = m_currentid + connectedId+2;
-        DescTextChange();
+        if(m_currentid == 3)
+        {
+            Mapae();
+        }
+        if (TextMgr.Inst.m_assetTexts[m_currentid - 1].category == 3)
+        {
+            m_endingScript.PrintEndingCredit(2);
+            //비활성화
+        }
+        else
+        {
+            m_currentid = m_currentid + connectedId + 1;
+            DescTextChange();
+        }
     }
+    public void Mapae()
+    {
+
+    }
+
     public void OnClick_Single()
     {
         int connectedId = TextMgr.Inst.m_assetTexts[m_currentid - 1].connectedid;
@@ -65,15 +92,20 @@ public class TextUIDlg : MonoBehaviour
             m_selectionText_1.gameObject.SetActive(false);
             m_selectionText_2.gameObject.SetActive(false);
             m_selectionTextSingle.gameObject.SetActive(true);
-            m_selectionTextSingle.text = "���";
+            m_selectionTextSingle.text = "계속";
             GameMgr.Inst.ginfo.m_lastSelection = m_currentid;
+        }
+        else if (selection_Num == 3)
+        {
+
         }
         else if (selection_Num == 4)
         {
             m_selectionText_1.gameObject.SetActive(false);
             m_selectionText_2.gameObject.SetActive(false);
             m_selectionTextSingle.gameObject.SetActive(true);
-            m_selectionTextSingle.text = "�� ����";
+            m_selectionTextSingle.text = "죽었습니다.";
+            m_selectionTextSingle.gameObject.transform.localScale = Vector3.one * 2;
             m_selectSingle.interactable = false;
             GameMgr.Inst.ginfo.m_lastSelection = m_currentid;
         }
