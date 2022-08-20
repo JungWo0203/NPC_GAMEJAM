13e595d55ca5fc2a06e2d50da7c8452657a9ba40
JungWo0203 <102477444+JungWo0203@users.noreply.github.com>
2022-08-21 08:37:19 +0900

    111
    111

--- Assets/Scripts/InventoryDlg.cs
+++ Assets/Scripts/InventoryDlg.cs
@@ -5,28 +5,28 @@ using UnityEngine.UI;
 
 public class InventoryDlg : MonoBehaviour
 {
-    public Button m_button_EnterLog=null;
-    public Button m_button_ExitLog=null;
-    public GameObject m_LogPanel = null;
-    public Text LogText = null;
+    public Button m_button_EnterOptions=null;
+    public Button m_button_ExitOptions=null;
+    public GameObject m_OptionPanel = null;
+    //public Text LogText = null;
     // Start is called before the first frame update
     public void Initialize()
     {
-        m_LogPanel.SetActive(false);
-        m_button_ExitLog.gameObject.SetActive(false);
-        m_button_EnterLog.onClick.AddListener(OnLogPress);
-        m_button_ExitLog.onClick.AddListener(OnLogExit);
+        m_OptionPanel.SetActive(false);
+        m_button_ExitOptions.gameObject.SetActive(false);
+        m_button_EnterOptions.onClick.AddListener(OnOptionPress);
+        m_button_ExitOptions.onClick.AddListener(OnOptionExit);
     }
-    void OnLogExit()
+    void OnOptionExit()
     {
-        m_LogPanel.SetActive(false);        
-        m_button_ExitLog.gameObject.SetActive(false);
+        m_OptionPanel.SetActive(false);        
+        m_button_ExitOptions.gameObject.SetActive(false);
     }
-    void OnLogPress()
+    void OnOptionPress()
     {
-        m_LogPanel.SetActive(true);
-        m_button_ExitLog.gameObject.SetActive(true);
-        LogText.text = TextMgr.Inst.m_assetTexts[GameMgr.Inst.ginfo.m_lastSelection-1].desc;
+        m_OptionPanel.SetActive(true);
+        m_button_ExitOptions.gameObject.SetActive(true);
+        //LogText.text = TextMgr.Inst.m_assetTexts[GameMgr.Inst.ginfo.m_lastSelection-1].desc;
     }
 
     // Update is called once per frame
